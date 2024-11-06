d16528fdde1f8b7275403e74f102a0e1133363db
JonathanGiegold <jonathan.giegold@hotmail.se>
2024-11-06 14:43:28 +0100

    startat

---
+++ sketch_robot/sketch_robot.ino
@@ -0,0 +1,19 @@
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
+    Serial.println(" En sekund har gått");
+  }
+  // put your main code here, to run repeatedly:
+
+}
